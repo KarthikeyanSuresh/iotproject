10380ebef5b9307f5e6344c0cf184d131ddeea7a
mdtorres21 <48358216+mdtorres21@users.noreply.github.com>
2022-12-08 11:20:38 +0100

    Update server.py
    Added mongodb

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017, username='username', password='password') put authentication
+
+db = client.flask_db
+todos = db.todos
 
 @app.route("/")
 def home():
