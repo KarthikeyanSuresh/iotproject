7954d910e66f35a9f259cc875d03a13d5c1a0708
KarthikeyanSuresh <karthikeyansuresh2018@gmail.com>
2022-12-16 10:37:53 +0100

    Added files for Docer from Ramazan's repo

---
+++ init-db.js
@@ -0,0 +1,20 @@
+db = db.getSiblingDB("db");
+db.table1.drop();
+
+db.table1.insertMany([
+    {
+        "id": 1,
+        "name": "Lion",
+        "type": "wild"
+    },
+    {
+        "id": 2,
+        "name": "Cow",
+        "type": "domestic"
+    },
+    {
+        "id": 3,
+        "name": "Tiger",
+        "type": "wild"
+    },
+]);
