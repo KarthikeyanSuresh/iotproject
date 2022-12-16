4e194bb04fc1d1c89663cc11fe0b5c42b534355b
mdtorres21 <48358216+mdtorres21@users.noreply.github.com>
2022-12-16 09:19:32 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,82 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 # client = MongoClient('localhost', 27017, username='username', password='password') put authentication
-
 db = client.flask_db
-todos = db.todos
+raw_data = db.raw_data
+
+#static data for now
+
+obj1 = {
+	"criteria": {
+		"Comfort" : {
+		     "Luminosity" : {
+                "Room 1" : 1,
+                "Room 2" : 4,
+                "Room 3" : 9
+             },
+		     "Temperature" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+		     "Noise" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             }
+		},
+		"Health": {
+			"CO2" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+		     "Humidity" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+		     "Air Pressure" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             }
+        },
+		"Usage":{
+			"Furniture" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+		     "Accessibility" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             }
+		}
+	}
+}
+
+ROOMS = [obj1]
 
 @app.route("/")
-def home():
-    return "Hello, World!"
-    
+def index():
+    return render_template('index.html')
+
+@app.route('/getfrom_rpi', methods=('GET', 'POST'))
+def create_record():
+    if request.method == 'POST':
+        print("post")
+        record = json.loads(request.data)
+        raw_data.insert_one(record)
+        return redirect(url_for('index')) 
+    print("get")
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
