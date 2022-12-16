5bfc60c658f2df860d4644344bba25bf805cdba5
KarthikeyanSuresh <karthikeyansuresh2018@gmail.com>
2022-12-16 08:50:33 +0100

    Changes to make functions work with cleanvalues.py

--- ahp.py
+++ ahp.py
@@ -1,6 +1,7 @@
 import ahpy
 import json
 import itertools
+from cleanvalues import modelValues, cleanValues
 
 def make_pairs(crit):
    return list(itertools.combinations(crit, 2))
@@ -16,56 +17,59 @@ def get_values(val_dict, obj):
 
 
 # Sample JSON
-obj ={
-    "criteria": {
-        "Comfort": {
-            "Luminosity": {
-                "Room 1": 0.5,
-                "Room 2": 0.001,
-                "Room 3": 0.001
-            },
-            "Noise": {
-                "Room 1": 1,
-                "Room 2": 0.8,
-                "Room 3": 0.5
-            },
-            "Temperature": {
-                "Room 1": 0.001,
-                "Room 2": 0.5,
-                "Room 3": 0.1
-            }
+obj = {
+	"criteria": {
+		"Comfort" : {
+		     "Luminosity" : {
+                "Room 1" : 25,
+                "Room 2" : 4,
+                "Room 3" : 90
+             },
+		     "Temperature" : {
+                "Room 1" : 14,
+                "Room 2" : 24,
+                "Room 3" : 30
+             },
+		     "Noise" : {
+                "Room 1" : 55,
+                "Room 2" : 60,
+                "Room 3" : 80
+             }
+		},
+		"Health": {
+			"CO2" : {
+                "Room 1" : 400,
+                "Room 2" : 350,
+                "Room 3" : 1000
+             },
+		     "Humidity" : {
+                "Room 1" : 20,
+                "Room 2" : 30,
+                "Room 3" : 60
+             },
+		     "Air Pressure" : {
+                "Room 1" : 103000,
+                "Room 2" : 101325,
+                "Room 3" : 100000
+             }
         },
-        "Health": {
-            "Air Pressure": {
-                "Room 1": 0.5,
-                "Room 2": 0.8,
-                "Room 3": 0.5
-            },
-            "CO2": {
-                "Room 1": 1,
-                "Room 2": 1,
-                "Room 3": 0.8
-            },
-            "Humidity": {
-                "Room 1": 0.2,
-                "Room 2": 0.5,
-                "Room 3": 0.6
-            }
-        },
-        "Usage": {
-            "Accessibility": {
-                "Room 1": 0.7,
-                "Room 2": 1,
-                "Room 3": 0.8
-            },
-            "Furniture": {
-                "Room 1": 1,
-                "Room 2": 0.7,
-                "Room 3": 0.6
-            }
-        }
-    }
+		"Usage":{
+			"Furniture" : {
+                "Room 1" : 1,
+                "Room 2" : 0.7,
+                "Room 3" : 0.6
+             },
+		     "Accessibility" : {
+                "Room 1" : 0.7,
+                "Room 2" : 1,
+                "Room 3" : 0.8
+             }
+		}
+	}
 }
+
+modelValues(obj)
+cleanValues(obj)
 
 room = obj['criteria']['Comfort']['Luminosity'].keys()
 
@@ -129,3 +133,5 @@ hierarchy = {'Criteria': ['Comfort', 'Health', 'Usage'],
 compose.add_hierarchy(hierarchy)
 
 criteria_report = compose.report('Criteria', show=True)
+
+# print(json.dumps(obj, sort_keys=True, indent=4))
