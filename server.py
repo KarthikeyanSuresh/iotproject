a8b570428302baa126d78c6d0448a5b26f5483ad
KarthikeyanSuresh <38375444+KarthikeyanSuresh@users.noreply.github.com>
2023-01-08 15:42:14 +0100

    Updated from other repo

--- server.py
+++ server.py
@@ -1,65 +1,68 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
+import os
 from pymongo import MongoClient
 from flask_mqtt import Mqtt
 import json
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-# client = MongoClient('localhost', 27017, username='username', password='password') # put authentication
-db = client.local
+#static data for now
+client = MongoClient(host=os.environ['MONGODB_HOSTNAME'],
+                         port=27017, 
+                         username=os.environ['MONGODB_USERNAME'], 
+                         password=os.environ['MONGODB_PASSWORD'],
+                        authSource="admin")
+db = client.flask_db
 raw_data = db.raw_data
-
-# static data for now
 obj1 = {
-	"criteria": {
-		"Comfort" : {
-		     "Luminosity" : {
+    "criteria": {
+        "Comfort" : {
+             "Luminosity" : {
                 "Room 1" : 1,
                 "Room 2" : 4,
                 "Room 3" : 9
              },
-		     "Temperature" : {
+             "Temperature" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              },
-		     "Noise" : {
-                "Room 1" : 1,
-                "Room 2" : 1,
-                "Room 3" : 1
-             }
-		},
-		"Health": {
-			"CO2" : {
-                "Room 1" : 1,
-                "Room 2" : 1,
-                "Room 3" : 1
-             },
-		     "Humidity" : {
-                "Room 1" : 1,
-                "Room 2" : 1,
-                "Room 3" : 1
-             },
-		     "Air Pressure" : {
+             "Noise" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              }
         },
-		"Usage":{
-			"Furniture" : {
+        "Health": {
+            "CO2" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              },
-		     "Accessibility" : {
+             "Humidity" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+             "Air Pressure" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              }
-		}
-	}
+        },
+        "Usage":{
+            "Furniture" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+             "Accessibility" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             }
+        }
+    }
 }
 
 ROOMS = [obj1]
@@ -100,20 +103,35 @@ def publish_message():
    publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
    return jsonify({'code': publish_result[0]})  
 
+def get_db():
+    client = MongoClient(host=os.environ['MONGODB_HOSTNAME'],
+                         port=27017, 
+                         username=os.environ['MONGODB_USERNAME'], 
+                         password=os.environ['MONGODB_PASSWORD'],
+                        authSource="admin")
+    db = client["db"]
+    return db
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route('/getfrom_rpi', methods=('GET', 'POST'))
 def create_record():
-    print("test")
     if request.method == 'POST':
         print("post")
         record = json.loads(request.data)
         raw_data.insert_one(record)
-        return render_template('index.html')
+        return redirect(url_for('index')) 
     print("get")
     return render_template('index.html')
 
-if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=5000)
+@app.route('/table1')
+def get_table1():
+    db = get_db()
+    _table1 = db.table1.find()
+    table1 = [{"id": entry["id"], "name": entry["name"], "type": entry["type"]} for entry in _table1]
+    return jsonify({"table1": table1})
+
+if __name__=='__main__':
+    app.run(host="127.0.0.1", port=5000)
