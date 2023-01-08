e433e59dd4397329bcdb6e52cf1c85f88d7d6c6a
KarthikeyanSuresh <38375444+KarthikeyanSuresh@users.noreply.github.com>
2023-01-08 14:45:05 +0100

    Added MQTT in Flask app

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from pymongo import MongoClient
+from flask_mqtt import Mqtt
 import json
 
 app = Flask(__name__)
@@ -9,8 +10,7 @@ client = MongoClient('localhost', 27017)
 db = client.flask_db
 raw_data = db.raw_data
 
-#static data for now
-
+# static data for now
 obj1 = {
 	"criteria": {
 		"Comfort" : {
@@ -64,6 +64,42 @@ obj1 = {
 
 ROOMS = [obj1]
 
+
+# MQTT configuration
+
+app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
+app.config['MQTT_BROKER_PORT'] = 1883
+# app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
+# app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
+app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
+topic = '/flask/mqtt'
+mqtt_client = Mqtt(app)
+
+
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+   if rc == 0:
+       print('Connected successfully')
+       mqtt_client.subscribe(topic) # subscribe topic
+   else:
+       print('Bad connection. Code:', rc)
+
+@mqtt_client.on_message()
+def handle_mqtt_message(client, userdata, message):
+   data = dict(
+       topic=message.topic,
+       payload=message.payload.decode()
+  )
+   print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+
+
+@app.route('/publish', methods=['POST'])
+def publish_message():
+   request_data = request.get_json()
+   publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
+   return jsonify({'code': publish_result[0]})  
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -78,5 +114,5 @@ def create_record():
     print("get")
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   app.run(host='127.0.0.1', port=5000)
