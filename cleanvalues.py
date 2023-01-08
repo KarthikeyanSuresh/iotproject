21c47606111b3ae17d64468594b148228631c2e5
KarthikeyanSuresh <38375444+KarthikeyanSuresh@users.noreply.github.com>
2023-01-08 16:40:30 +0100

    updated changes from other repo

--- cleanvalues.py
+++ cleanvalues.py
@@ -3,56 +3,60 @@ import random
 
 
 # Static JSON object, to be imported from Arduino
-obj = {
-	"criteria": {
-		"Comfort" : {
-		     "Luminosity" : {
-                "Room 1" : 25,
-                "Room 2" : 4,
-                "Room 3" : 90
-             },
-		     "Temperature" : {
-                "Room 1" : 14,
-                "Room 2" : 24,
-                "Room 3" : 30
-             },
-		     "Noise" : {
-                "Room 1" : 55,
-                "Room 2" : 60,
-                "Room 3" : 80
-             }
-		},
-		"Health": {
-			"CO2" : {
-                "Room 1" : 400,
-                "Room 2" : 350,
-                "Room 3" : 1000
-             },
-		     "Humidity" : {
-                "Room 1" : 20,
-                "Room 2" : 30,
-                "Room 3" : 60
-             },
-		     "Air Pressure" : {
-                "Room 1" : 103000,
-                "Room 2" : 101325,
-                "Room 3" : 100000
-             }
-        },
-		"Usage":{
-			"Furniture" : {
-                "Room 1" : 1,
-                "Room 2" : 0.7,
-                "Room 3" : 0.6
-             },
-		     "Accessibility" : {
-                "Room 1" : 0.7,
-                "Room 2" : 1,
-                "Room 3" : 0.8
-             }
-		}
-	}
-}
+def createObject(msg1, msg2, msg3):
+
+    obj = {
+        "criteria": {
+            "Comfort" : {
+                "Luminosity" : {
+                    "Room 1" : msg1["light"],
+                    "Room 2" : msg2["light"],
+                    "Room 3" : msg3["light"]
+                },
+                "Temperature" : {
+                    "Room 1" : msg1["temperature"],
+                    "Room 2" : msg2["temperature"],
+                    "Room 3" : msg3["temperature"]
+                },
+                "Noise" : {
+                    "Room 1" : msg1["noise"],
+                    "Room 2" : msg2["noise"],
+                    "Room 3" : msg3["noise"]
+                }
+            },
+            "Health": {
+                "CO2" : {
+                    "Room 1" : 400,
+                    "Room 2" : 350,
+                    "Room 3" : 1000
+                },
+                "Humidity" : {
+                    "Room 1" : msg1["humidity"],
+                    "Room 2" : msg2["humidity"],
+                    "Room 3" : msg3["humidity"]
+                },
+                "Air Pressure" : {
+                    "Room 1" : 103000,
+                    "Room 2" : 101325,
+                    "Room 3" : 100000
+                }
+            },
+            "Usage":{
+                "Furniture" : {
+                    "Room 1" : 1,
+                    "Room 2" : 0.7,
+                    "Room 3" : 0.6
+                },
+                "Accessibility" : {
+                    "Room 1" : 0.7,
+                    "Room 2" : 1,
+                    "Room 3" : 0.8
+                }
+            }
+        }
+    }
+    return obj
+
 
 def modelValues(obj):
     # Creating and adding modeled data for CO2 and Airpressure
@@ -170,7 +174,63 @@ def cleanValues(obj):
 
 
 
-modelValues(obj)
-cleanValues(obj)
+# modelValues(obj)
+# cleanValues(obj)
 
 # print(json.dumps(obj, sort_keys=True, indent=4))
+
+
+
+
+
+
+# obj = {
+# 	"criteria": {
+# 		"Comfort" : {
+# 		     "Luminosity" : {
+#                 "Room 1" : 25,
+#                 "Room 2" : 4,
+#                 "Room 3" : 90
+#              },
+# 		     "Temperature" : {
+#                 "Room 1" : 14,
+#                 "Room 2" : 24,
+#                 "Room 3" : 30
+#              },
+# 		     "Noise" : {
+#                 "Room 1" : 55,
+#                 "Room 2" : 60,
+#                 "Room 3" : 80
+#              }
+# 		},
+# 		"Health": {
+# 			"CO2" : {
+#                 "Room 1" : 400,
+#                 "Room 2" : 350,
+#                 "Room 3" : 1000
+#              },
+# 		     "Humidity" : {
+#                 "Room 1" : 20,
+#                 "Room 2" : 30,
+#                 "Room 3" : 60
+#              },
+# 		     "Air Pressure" : {
+#                 "Room 1" : 103000,
+#                 "Room 2" : 101325,
+#                 "Room 3" : 100000
+#              }
+#         },
+# 		"Usage":{
+# 			"Furniture" : {
+#                 "Room 1" : 1,
+#                 "Room 2" : 0.7,
+#                 "Room 3" : 0.6
+#              },
+# 		     "Accessibility" : {
+#                 "Room 1" : 0.7,
+#                 "Room 2" : 1,
+#                 "Room 3" : 0.8
+#              }
+# 		}
+# 	}
+# }
--- server.py
+++ server.py
@@ -1,68 +1,66 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
-import os
 from pymongo import MongoClient
 from flask_mqtt import Mqtt
 import json
+from cleanvalues import createObject
 
 app = Flask(__name__)
 
-#static data for now
-client = MongoClient(host=os.environ['MONGODB_HOSTNAME'],
-                         port=27017, 
-                         username=os.environ['MONGODB_USERNAME'], 
-                         password=os.environ['MONGODB_PASSWORD'],
-                        authSource="admin")
-db = client.flask_db
+client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017, username='username', password='password') # put authentication
+db = client.local
 raw_data = db.raw_data
+
+# static data for now
 obj1 = {
-    "criteria": {
-        "Comfort" : {
-             "Luminosity" : {
+	"criteria": {
+		"Comfort" : {
+		     "Luminosity" : {
                 "Room 1" : 1,
                 "Room 2" : 4,
                 "Room 3" : 9
              },
-             "Temperature" : {
+		     "Temperature" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              },
-             "Noise" : {
+		     "Noise" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             }
+		},
+		"Health": {
+			"CO2" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+		     "Humidity" : {
+                "Room 1" : 1,
+                "Room 2" : 1,
+                "Room 3" : 1
+             },
+		     "Air Pressure" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              }
         },
-        "Health": {
-            "CO2" : {
+		"Usage":{
+			"Furniture" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              },
-             "Humidity" : {
-                "Room 1" : 1,
-                "Room 2" : 1,
-                "Room 3" : 1
-             },
-             "Air Pressure" : {
+		     "Accessibility" : {
                 "Room 1" : 1,
                 "Room 2" : 1,
                 "Room 3" : 1
              }
-        },
-        "Usage":{
-            "Furniture" : {
-                "Room 1" : 1,
-                "Room 2" : 1,
-                "Room 3" : 1
-             },
-             "Accessibility" : {
-                "Room 1" : 1,
-                "Room 2" : 1,
-                "Room 3" : 1
-             }
-        }
-    }
+		}
+	}
 }
 
 ROOMS = [obj1]
@@ -70,13 +68,13 @@ ROOMS = [obj1]
 
 # MQTT configuration
 
-app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
+app.config['MQTT_BROKER_URL'] = 'test.mosquitto.org'
 app.config['MQTT_BROKER_PORT'] = 1883
 # app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
 # app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
-app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_KEEPALIVE'] = 60  # Set KeepAlive time in seconds
 app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
-topic = '/flask/mqtt'
+topic = 'rooms/#'
 mqtt_client = Mqtt(app)
 
 
@@ -103,35 +101,20 @@ def publish_message():
    publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
    return jsonify({'code': publish_result[0]})  
 
-def get_db():
-    client = MongoClient(host=os.environ['MONGODB_HOSTNAME'],
-                         port=27017, 
-                         username=os.environ['MONGODB_USERNAME'], 
-                         password=os.environ['MONGODB_PASSWORD'],
-                        authSource="admin")
-    db = client["db"]
-    return db
-
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route('/getfrom_rpi', methods=('GET', 'POST'))
 def create_record():
+    print("test")
     if request.method == 'POST':
         print("post")
         record = json.loads(request.data)
         raw_data.insert_one(record)
-        return redirect(url_for('index')) 
+        return render_template('index.html')
     print("get")
     return render_template('index.html')
 
-@app.route('/table1')
-def get_table1():
-    db = get_db()
-    _table1 = db.table1.find()
-    table1 = [{"id": entry["id"], "name": entry["name"], "type": entry["type"]} for entry in _table1]
-    return jsonify({"table1": table1})
-
-if __name__=='__main__':
-    app.run(host="127.0.0.1", port=5000)
+if __name__ == '__main__':
+   app.run(host='127.0.0.1', port=5000)
