669c039cc9d888522422478b1573031b8f7dea7e
KarthikeyanSuresh <38375444+KarthikeyanSuresh@users.noreply.github.com>
2023-01-09 18:51:52 +0100

    Added modularization

--- utilities.py
+++ utilities.py
@@ -175,65 +175,10 @@ def cleanValues(obj):
                         obj["criteria"][keys][key][room] = 0.001
 
 
-
-# createObject(msg1, msg2, msg3)
-# modelValues(obj)
-# cleanValues(obj)
-
-# print(json.dumps(obj, sort_keys=True, indent=4))
-
+def processRawData(msg1, msg2, msg3):
+    obj = createObject(msg1, msg2, msg3)
+    # print(json.dumps(obj, sort_keys=True, indent=4))
+    modelValues(obj)
+    cleanValues(obj)
 
 
-
-
-
-# obj = {
-# 	"criteria": {
-# 		"Comfort" : {
-# 		     "Luminosity" : {
-#                 "Room 1" : 25,
-#                 "Room 2" : 4,
-#                 "Room 3" : 90
-#              },
-# 		     "Temperature" : {
-#                 "Room 1" : 14,
-#                 "Room 2" : 24,
-#                 "Room 3" : 30
-#              },
-# 		     "Noise" : {
-#                 "Room 1" : 55,
-#                 "Room 2" : 60,
-#                 "Room 3" : 80
-#              }
-# 		},
-# 		"Health": {
-# 			"CO2" : {
-#                 "Room 1" : 400,
-#                 "Room 2" : 350,
-#                 "Room 3" : 1000
-#              },
-# 		     "Humidity" : {
-#                 "Room 1" : 20,
-#                 "Room 2" : 30,
-#                 "Room 3" : 60
-#              },
-# 		     "Air Pressure" : {
-#                 "Room 1" : 103000,
-#                 "Room 2" : 101325,
-#                 "Room 3" : 100000
-#              }
-#         },
-# 		"Usage":{
-# 			"Furniture" : {
-#                 "Room 1" : 1,
-#                 "Room 2" : 0.7,
-#                 "Room 3" : 0.6
-#              },
-# 		     "Accessibility" : {
-#                 "Room 1" : 0.7,
-#                 "Room 2" : 1,
-#                 "Room 3" : 0.8
-#              }
-# 		}
-# 	}
-# }
